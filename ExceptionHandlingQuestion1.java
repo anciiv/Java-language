8ab1e6a193afc2127e58f5069141358bc9a9dd26
anciiv <76643257+anciiv@users.noreply.github.com>
2022-09-08 17:22:24 +0200

    Add files via upload

---
+++ ExceptionHandlingQuestion1.java
@@ -0,0 +1,48 @@
+package quiz;
+
+public class ExceptionHandlingQuestion1 {
+
+	 public static void main(String args[]) { 
+//	      try { 
+//	         throw 10; 
+//	      } 
+//	      catch(int e) { 
+//	         System.out.println("Got the  Exception " + e); 
+//	      } 
+	  } 
+	 
+	 /*
+	  * (A) Got the Exception 10
+		(B) Got the Exception 0
+		(C) Compiler Error
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		Answer: (C) 
+
+		Explanation: In Java only throwable objects (Throwable objects are instances of any subclass of the Throwable class) 
+		can be thrown as exception. So basic data type can no be thrown at all.
+	  */
+}
